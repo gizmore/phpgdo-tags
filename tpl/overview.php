607512e7947505fa5f10c79e82fc48b3c2e66480
gizmore <gizmore@wechall.net>
2022-09-02 03:12:47 +0200

    '7.0.1-r1563 fineprint.phpgdo.com passes all unit tests'

--- tpl/overview.php
+++ tpl/overview.php
@@ -3,4 +3,4 @@ use GDO\UI\GDT_Panel;
 use GDO\UI\GDT_Bar;
 /** @var $navbar GDT_Bar **/
 echo $navbar->render();
-echo GDT_Panel::make()->html(t('box_content_tags_overview'))->render();
+echo GDT_Panel::make()->text('box_content_tags_overview')->render();
