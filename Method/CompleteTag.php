9878a1231137aa8df647427774138b835957f35e
gizmore <gizmore@wechall.net>
2022-10-02 04:18:25 +0200

    '7.0.1-r1635'

--- Method/CompleteTag.php
+++ Method/CompleteTag.php
@@ -7,28 +7,17 @@ use GDO\Tags\GDO_Tag;
 
 final class CompleteTag extends MethodCompletion
 {
-    public function gdoTable() : GDO
-    {
-        return GDO_Tag::table();
-    }
 
-    public function gdoHeaderColumns()
-    {
-        return ['tag_name'];
-    }
-    
-    public function renderJSON(GDO $gdo)
-    {
-        return array(
-            'id' => $gdo->getID(),
-            'text' => $gdo->displayName(),
-            'display' => $gdo->renderHTML(),
-        );
-    }
-	public function execute()
+	public function gdoTable(): GDO
 	{
+		return GDO_Tag::table();
 	}
 
-
+	protected function gdoHeaderFields(): array
+	{
+		return [
+			'tag_name',
+		];
+	}
 
 }
